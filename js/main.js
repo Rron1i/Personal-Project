7237e61e16fab0e959398f80ecc77423fad1b65c
Rron1i <>
2025-04-10 17:27:10 +0200

    need more tweaks to img

--- js/main.js
+++ js/main.js
@@ -9,10 +9,39 @@ function isMobileDevice() {
 if (!isMobileDevice() && movingProfile) {
     window.addEventListener('scroll', function() {
 
-        if (window.scrollY >= 150) {
+        if (window.scrollY >= 530) {
             movingProfile.style.transform = "translateY(260px)";
         } else {
             movingProfile.style.transform = "translateY(0)";
         }
     });
 }
+
+
+var slideIndex = 1;
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
+showSlides(1);
+
+function plusSlides(n) {
+    slideIndex += n;
+    showSlides(slideIndex);
+}
+
+setInterval(plusSlides, 3000, 1);
