cc623cf4c810935d79df861421a561ed991f7ccd
Rron1i <>
2025-04-15 17:32:36 +0200

    maybe the end? theres still about file

--- js/main.js
+++ js/main.js
@@ -61,15 +61,18 @@ resetAutoSlide();
 
     openBtn.addEventListener('click', () => {
       modal.style.display = 'flex';
+      document.body.classList.add("no-scroll");
     });
 
     closeBtn.addEventListener('click', () => {
       modal.style.display = 'none';
+      document.body.classList.remove("no-scroll");
     });
 
-    // Close modal when clicking outside content
+    // close modal when clicking outside content
     window.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = 'none';
+        document.body.classList.remove("no-scroll");
       }
     });
